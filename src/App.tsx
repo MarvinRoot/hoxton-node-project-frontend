51638816596b4cbb80079ad6a4208c904a5390b7
Marvin Aliaj <74553145+MarvinRoot@users.noreply.github.com>
2022-03-21 17:41:58 +0100

    finished most of the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './styling/App.css'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Home } from './pages/Home'
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Route index element={<Navigate replace to="sign-in" />} />
         <Route path='/sign-in' element={< SignIn />} />
         <Route path='/sign-up' element={< SignUp />} />
+        <Route path='/home' element={< Home />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import { Header } from "../components/Header"
+
+export function Home() {
+    return (
+        <section className="home">
+            <Header/>
+            <section className="main">
+                <div className="empty-space"></div>
+                <div className="main-content">
+                    <h1>Main content</h1>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                    <div className="question-card">
+                        <p>@username</p>
+                        <h4>Some question</h4>
+                        <p>2022/02/02</p>
+                    </div>
+                </div>
+                <div className="empty-space"></div>
+            </section>
+        </section>
+
+    )
+}
